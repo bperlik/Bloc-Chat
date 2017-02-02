8ddde92f845577b98b6afc1429855f8cea8c7be1
Barbara Perlik <barbperlik@gmail.com>
2017-02-02 13:14:41 -0500

    Added Room creation, but no modal form

--- app/scripts/services/Room.js
+++ app/scripts/services/Room.js
@@ -2,9 +2,16 @@
   function Room($firebaseArray) {
     var ref = firebase.database().ref().child("rooms");
     var rooms = $firebaseArray(ref);
-
+	 var newRoomName = 'Test Room';
+	 
+	 rooms.$add({name: newRoomName}).then(function(ref) {
+		 var id = ref.key;
+		 console.log("Added room " + newRoomName);
+		 rooms.$indexFor(id); //returns location of new room in array
+	 });
+		 	    
     return {
-      all: rooms
+      all: rooms 
     };
   }
 
