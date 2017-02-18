bab9cd817d570183e7dd1dc4cec37a99be91fba9
Barbara Perlik <barbperlik@gmail.com>
2017-02-18 10:32:18 -0500

    set username with cookies

--- app/scripts/services/Message.js
+++ app/scripts/services/Message.js
@@ -15,15 +15,3 @@
 		.module('blocChat')
 		.factory('Message', ['$firebaseArray', Message]);
 })();
-
-
-//.orderByChild("roomId").equalTo(roomId)
-
-/*
-example database Object in messages 
-$id: "-Kc_08KBBrwc3nL0Q4_w"
-$priority: null
-content: ""This is another message."
-sentAt: "Tue Feb 15 2017 08:57:03 GMT 0900 EDT"
-username: "Jack B Nimble"
-*/
